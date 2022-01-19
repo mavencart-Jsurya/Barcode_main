31d64cce0fdaff619270f163512cd84318da32d6
mavencart-Jsurya <97090065+mavencart-Jsurya@users.noreply.github.com>
2022-01-19 17:58:11 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,19 @@
+
+/* eslint-disable no-undef */
+import "./style.css"
+
+function App() {
+//   const call = (res)=>{
+//  console.log("testing", res)
+//   }
+  return (
+   <div className="app">
+    <header> 
+   <button  onClick={()=>mwbScanner.startScanning(200,100)}>Scan</button>
+
+  </header>     
+   </div>
+  )
+}
+
+export default App;
